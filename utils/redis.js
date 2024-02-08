64dcf8a2af8f59137206bc34fa5afe9de50df2a5
IdiJr <idialijnr@gmail.com>
2024-02-08 12:36:38 +0000

    Redis utils

---
+++ utils/redis.js
@@ -0,0 +1,61 @@
+import redis from 'redis';
+
+// class to define methods for commonly used redis commands
+class RedisClient {
+  constructor () {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.error('Redis client error:', error);
+    });
+  }
+
+  // check connection status and report
+  isAlive () {
+    if (this.client.connected) {
+      return true;
+    }
+    return false;
+  }
+
+  // get value for given key from redis server
+  async get (key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  // set key value pair to redis server
+  async set (key, value, durationInSeconds) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, durationInSeconds, value, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+
+  // del key vale pair from redis server
+  async del (key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+export default redisClient;
