2c287bdecb51ff45857b34e52e6251c71f9c83fc
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-11 20:59:09 +0000

    Redis

--- utils/redis.js
+++ utils/redis.js
@@ -2,7 +2,7 @@ import redis from 'redis';
 
 // class to define methods for commonly used redis commands
 class RedisClient {
-  constructor () {
+  constructor() {
     this.client = redis.createClient();
     this.client.on('error', (error) => {
       console.error('Redis client error:', error);
@@ -10,7 +10,7 @@ class RedisClient {
   }
 
   // check connection status and report
-  isAlive () {
+  isAlive() {
     if (this.client.connected) {
       return true;
     }
@@ -18,7 +18,7 @@ class RedisClient {
   }
 
   // get value for given key from redis server
-  async get (key) {
+  async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (error, value) => {
         if (error) {
@@ -31,7 +31,7 @@ class RedisClient {
   }
 
   // set key value pair to redis server
-  async set (key, value, durationInSeconds) {
+  async set(key, value, durationInSeconds) {
     return new Promise((resolve, reject) => {
       this.client.setex(key, durationInSeconds, value, (error, result) => {
         if (error) {
@@ -44,7 +44,7 @@ class RedisClient {
   }
 
   // del key vale pair from redis server
-  async del (key) {
+  async del(key) {
     return new Promise((resolve, reject) => {
       this.client.del(key, (error, result) => {
         if (error) {
