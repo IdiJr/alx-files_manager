e14e801f1d934a129d74254e2f45b970d1d47547
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-11 21:25:39 +0000

    first Api

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,17 +2,17 @@ import dbClient from '../utils/db.js';
 import redisClient from '../utils/redis.js';
 
 const AppController = {
-  async getStatus (req, res) {
+  async getStatus(req, res) {
     const redisStatus = redisClient.isAlive();
     const dbStatus = dbClient.isAlive();
     res.status(200).json({ redis: redisStatus, db: dbStatus });
   },
 
-  async getStats (req, res) {
+  async getStats(req, res) {
     const usersCount = await dbClient.nbUsers();
     const filesCount = await dbClient.nbFiles();
     res.status(200).json({ users: usersCount, files: filesCount });
-  }
+  },
 };
 
 export default AppController;
