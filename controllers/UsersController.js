c017d57cc2f171f8544ee0b3e1eda2d8f0262988
IdiJr <idialijnr@gmail.com>
2024-02-12 01:18:21 +0000

    Authenticate a user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,9 +1,10 @@
 import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js';
 import { hash } from 'bcrypt';
 import { v4 as uuidv4 } from 'uuid';
 
 const UsersController = {
-  async postNew(req, res) {
+  async postNew (req, res) {
     const { email, password } = req.body;
 
     if (!email) {
@@ -23,12 +24,31 @@ const UsersController = {
     const newUser = {
       email,
       password: hashedPassword,
-      id: uuidv4(),
+      id: uuidv4()
     };
 
     const savedUser = await dbClient.createUser(newUser);
     return res.status(201).json({ id: savedUser.id, email: savedUser.email });
   },
+
+  async getMe (req, res) {
+    const { 'x-token': token } = req.headers;
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.getUserById(userId);
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
+  }
 };
 
 export default UsersController;
