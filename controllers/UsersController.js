4c88e7b1319aff42bea1379b6b6400e6a52437a5
emmanueladdo <asememmanueladdo@gmail.com>
2024-02-14 23:15:33 +0000

    user control

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,53 +1,63 @@
 import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
+import Queue from 'bull';
 import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
-const UsersController = {
-  async postNew(req, res) {
-    const { email, password } = req.body;
+const userQueue = new Queue('userQueue', 'redis://127.0.0.1:6379');
+
+class UsersController {
+  static postNew(request, response) {
+    const { email } = request.body;
+    const { password } = request.body;
 
     if (!email) {
-      return res.status(400).json({ error: 'Missing email' });
+      response.status(400).json({ error: 'Missing email' });
+      return;
     }
-
     if (!password) {
-      return res.status(400).json({ error: 'Missing password' });
+      response.status(400).json({ error: 'Missing password' });
+      return;
     }
 
-    // Check if the email already exists in the Redis cache
-    const emailExistsInCache = await redisClient.get(email);
-    if (emailExistsInCache) {
-      return res.status(400).json({ error: 'Already exist' });
+    const users = dbClient.db.collection('users');
+    users.findOne({ email }, (err, user) => {
+      if (user) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        const hashedPassword = sha1(password);
+        users.insertOne(
+          {
+            email,
+            password: hashedPassword,
+          },
+        ).then((result) => {
+          response.status(201).json({ id: result.insertedId, email });
+          userQueue.add({ userId: result.insertedId });
+        }).catch((error) => console.log(error));
+      }
+    });
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      users.findOne({ _id: idObject }, (err, user) => {
+        if (user) {
+          response.status(200).json({ id: userId, email: user.email });
+        } else {
+          response.status(401).json({ error: 'Unauthorized' });
+        }
+      });
+    } else {
+      console.log('Not found!');
+      response.status(401).json({ error: 'Unauthorized' });
     }
+  }
+}
 
-    // Check if the email already exists in the MongoDB database
-    const existingUser = await dbClient.client.db().collection('users').findOne({ email });
-    if (existingUser) {
-      // Cache the email to Redis to avoid duplicate registrations
-      await redisClient.set(email, 'true', 60); // 60 seconds cache
-      return res.status(400).json({ error: 'Already exist' });
-    }
-
-    // Hash the password using SHA1
-    const hashedPassword = sha1(password);
-
-    // Create a new user object
-    const newUser = {
-      email,
-      password: hashedPassword,
-    };
-
-    // Insert the new user into the users collection
-    const result = await dbClient.client.db().collection('users').insertOne(newUser);
-
-    // Return the new user with only the email and the id
-    const responseUser = {
-      id: result.insertedId,
-      email: newUser.email,
-    };
-
-    return res.status(201).json(responseUser);
-  },
-};
-
-export default UsersController;
+module.exports = UsersController;
